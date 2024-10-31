98d0e5dc16785f58ec57df2297d3a8dbf274f773
bnhwng <hungmilky@gmail.com>
2024-10-31 16:05:33 +1100

    simple fib

---
+++ src/DAS.java
@@ -0,0 +1,47 @@
+public class DAS {
+  public DAS() {
+  }
+
+  public int[] fibonacci() {
+    int[] result = new int[30];
+    result[0] = 0;
+    result[1] = 1;
+    for (int i = 2; i < 18; i++) {
+      result[i] = result[i - 1] + result[i - 2];
+    }
+    return result;
+  }
+
+  public int fibonacciTh(int th){
+    if(th <= 1){
+      return 1;
+    } else {
+      return fibonacciTh(th-1)+fibonacciTh(th-2);
+    }
+  }
+
+  public int[] fibArrUpto(int i){
+    int[] result = new int[i];
+    if(i <= 1){
+      return new int[]{1};
+    }
+
+    for(int j = 0; j<i; j++){
+      result[j]+=fibonacciTh(j);
+    }
+
+    return result;
+  }
+
+
+
+  public static void main(String[] args) {
+    DAS das = new DAS();
+    int[]result=das.fibArrUpto(20);
+    int i=0;
+    while(i<20){
+      System.out.println(result[i]);
+      i++;
+    }
+  }
+}
